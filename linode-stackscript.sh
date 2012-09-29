ed6426db4faf3d257ac03766fa184da282d83931
Ben Emson <ben@emson.co.uk>
2012-09-29 15:46:56 +0100

    Fixed wget script

--- linode-stackscript.sh
+++ linode-stackscript.sh
@@ -14,7 +14,7 @@ echo $HOSTNAME > /etc/hostname
 echo $IPADDR $HOSTNAME $FQDN >> /etc/hosts
 
 # get and execute the puppet script
-wget https://raw.github.com/lesscoding/lesscoding_puppet_scripts/master/linode-bootstrap.sh --output-file=/opt/linode-bootstrap.sh
+wget https://raw.github.com/lesscoding/lesscoding_puppet_scripts/master/linode-bootstrap.sh --output-document=/opt/linode-bootstrap.sh
 chmod +x /opt/linode-bootstrap.sh
 
 ./opt/linode-bootstrap.sh
