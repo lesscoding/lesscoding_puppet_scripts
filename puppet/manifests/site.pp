86a4dc6b254cfaa5634c102b1ff4a4fb20fb3663
Ben Emson <ben@emson.co.uk>
2012-11-19 14:59:20 +0000

    Added read from CSV file functionality

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -2,13 +2,16 @@ Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 }
 
-$app_dir          = "/var/www/lesscoding_com/"
-$root_db_password = "mypassword"
-$db_name          = "wp_lesscoding_com"
-$db_user          = "lesscodingdb"
-$db_password      = "apassword"
-$email_account    = "ben@lesscoding.com"
-$email_password   = "email_password"
+$extlookup_datadir = "/etc/puppet/manifests/extdata"
+$extlookup_precedence = ["%{fqdn}", "domain_%{domain}", "settings"]
+
+$app_dir          = extlookup("app_dir"          , "/var/app/my_app_dir/")
+$root_db_password = extlookup("root_db_password" , "root_db_password")
+$db_name          = extlookup("db_name"          , "wordpress_db_name")
+$db_user          = extlookup("db_user"          , "db_user")
+$db_password      = extlookup("db_password"      , "db_password")
+$email_account    = extlookup("email_account"    , "my_name@somewhere.com")
+$email_password   = extlookup("email_password"   , "email_password")
 
 include timezone
 include ntp
